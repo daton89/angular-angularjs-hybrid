13c1b7e9c04f8ca9218d5334b7ad19a319107e7d
Tony D'Angelo <tonydangelo123@gmail.com>
2019-09-17 15:35:00 +0200

    upgrade angularjs app to use it in angular

---
+++ src/app/angularjs/home/home.module.ts
@@ -0,0 +1,12 @@
+import HomePage from './home-page';
+
+declare var angular: angular.IAngularStatic;
+
+export default angular.module('angularjsApp.homePage', [])
+  .component('homePage', {
+    controller: HomePage,
+    bindings: {
+      message: '@'
+    },
+    template: require('html-loader!./home-page.html')
+  }).name;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UpgradeModule } from '@angular/upgrade/static';
 
-import { AppComponent } from './app.component';
+import('angular');
+import('angular-animate');
+import('angular-aria');
+import('angular-material');
+import('angular-resource');
+import('angular-sanitize');
+import('angular-ui-router');
+
+import homePage from './angularjs/home/home.module';
+import { ArticleListComponent } from './article/article-list/article-list.component';
+
+declare var angular: angular.IAngularStatic;
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    // AppComponent
+  ArticleListComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) { }
+  ngDoBootstrap() {
+    this.upgrade.bootstrap(document.body, ['angularjsApp']);
+  }
+}
+
+angular
+  .module('angularjsApp', [
+    homePage
+  ]);
